35116258bb36547eb92f2e362f402d82b6652f21
A khan <awal.khan@live.co.uk>
2024-07-29 23:58:17 +0000

    calculator updated

--- main.py
+++ main.py
@@ -18,19 +18,18 @@ operations = {
   "/":divide
 }
 num1 = int(input("What's the first number?: "))
-num2 = int(input("What's the second number?: "))
            
 for symbol in operations:
   print(symbol)
+should_continue = True
+while should_continue:
+  operation_symbol = input("Pick an operation : ")
+  num2 = int(input("What's the next number?: "))
+  calculation_function = operations[operation_symbol]
+  answer = calculation_function(num1,num2)
+  print(f"{num1} {operation_symbol} {num2} = {answer}")
 
-operation_symbol = input("Pick an operation from the line above: ")
-
-calculation_function = operations[operation_symbol]
-first_answer = calculation_function(num1,num2)
-print(f"{num1}, {operation_symbol}, {num2}, = {first_answer}")
-
-operation_symbol= input("pick another operation: ")
-num3 = int(input("What's the next number?: "))
-calculation_function = operations[operation_symbol]
-second_answer = calculation_function(calculation_function(num1,num2),num3)
-print(f"{first_answer} {operation_symbol} {num3} = {second_answer}")
+  if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to exit: ") == "y":
+    num1 = answer
+  else:
+    should_continue = False
